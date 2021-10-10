d0829412a81e6856b42b5d751d3b96425ad6db67
Swarajballal <86348862+Swarajballal@users.noreply.github.com>
2021-10-10 23:37:36 +0530

    Created Positive number in a list
    Write a Python program to print all positive numbers in a range.

---
+++ positivenum.py
@@ -0,0 +1,11 @@
+Write a Python program to print all positive numbers in a range.
+list1 = [12, -7, 5, 64, -14]
+list2 = [12, 14, -95, 3]
+for i in list1: 
+    if i >=0:
+       print("Positive numbers in list1 is",i)
+       
+for j in list2: 
+    if j >=0:
+       print("Positive numbers in list2 is",j)
+             
